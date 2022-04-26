ee59dbd86d767e6c1458c938dd12c21805276b2b
ITharu123 <89218316+ITharu123@users.noreply.github.com>
2022-04-26 17:02:03 +0530

    Customer Service New

--- ElectroGrid-Group-182/src/com/service/CustomerService.java
+++ ElectroGrid-Group-182/src/com/service/CustomerService.java
@@ -13,82 +13,96 @@ import com.model.Customer;
 import org.jsoup.*;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
+
 @Path("/Customer")
 public class CustomerService
 {
  Customer customerObj = new Customer();
 
+              //Insert Customer details
  
- @POST
- @Path("/")
- @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
- @Produces(MediaType.TEXT_PLAIN)
- public String insertCustomer(@FormParam("customerName") String customerName,
-  @FormParam("customerAddress") String customerAddress,
-  @FormParam("customerEmail") String customerEmail,
-  @FormParam("customerContact") String customerContact)
- {
-  String output = customerObj.insertCustomer(customerName, customerAddress , customerEmail , customerContact);
- return output;
- }
+              @POST
+              @Path("/")
+              @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+              @Produces(MediaType.TEXT_PLAIN)
+              public String insertCustomer(@FormParam("customerName") String customerName,
+               @FormParam("customerAddress") String customerAddress,
+               @FormParam("customerEmail") String customerEmail,
+               @FormParam("customerContact") String customerContact)
+              {
+               String output = customerObj.insertCustomer(customerName, customerAddress , customerEmail , customerContact);
+              return output;
+              }
+
+              //Read Customer Details
  
- @GET
- @Path("/")
- @Produces(MediaType.TEXT_HTML)
- public String readCustomers()
-  {
-  return customerObj.readCustomers();
- }
+              @GET
+              @Path("/")
+              @Produces(MediaType.TEXT_HTML)
+              public String readCustomers()
+               {
+               return customerObj.readCustomers();
+              }
  
- @PUT
- @Path("/")
- @Consumes(MediaType.APPLICATION_JSON)
- @Produces(MediaType.TEXT_PLAIN)
- public String updateCustomer(String customerData)
- {
- //Convert the input string to a JSON object
-  JsonObject customerObject = new JsonParser().parse(customerData).getAsJsonObject();
- //Read the values from the JSON object
-  String customerID = customerObject.get("customerID").getAsString();
-  String  customerName = customerObject.get("customerName").getAsString();
-  String customerAddress = customerObject.get("customerAddress").getAsString();
-  String customerEmail = customerObject.get("customerEmail").getAsString();
-  String customerContact = customerObject.get("customerContact").getAsString();
-  String output = customerObj.updateCustomer(customerID, customerName, customerAddress, customerEmail, customerContact);
- return output;
- }
+              
+              //Update Customer Details
  
- @DELETE
- @Path("/")
- @Consumes(MediaType.APPLICATION_XML)
- @Produces(MediaType.TEXT_PLAIN)
- public String deleteCustomer(String CustomerData)
- {
- //Convert the input string to an XML document
-  Document doc = Jsoup.parse(CustomerData, "", Parser.xmlParser());
+              @PUT
+              @Path("/")
+              @Consumes(MediaType.APPLICATION_JSON)
+              @Produces(MediaType.TEXT_PLAIN)
 
- //Read the value from the element <itemID>
-  String customerID = doc.select("customerID").text();
-  String output = customerObj.deleteCustomer(customerID);
- return output;
- }
+              public String updateCustomer(String customerData)
+              {
+              //Convert the input string to a JSON object
+               JsonObject customerObject = new JsonParser().parse(customerData).getAsJsonObject();
+               
+              //Read the values from the JSON object
+               String customerID = customerObject.get("customerID").getAsString();
+               String  customerName = customerObject.get("customerName").getAsString();
+               String customerAddress = customerObject.get("customerAddress").getAsString();
+               String customerEmail = customerObject.get("customerEmail").getAsString();
+               String customerContact = customerObject.get("customerContact").getAsString();
+               String output = customerObj.updateCustomer(customerID, customerName, customerAddress, customerEmail, customerContact);
+              return output;
+              }
  
-//view profile details
-@GET
-@Path("/profile/{customerID}")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.TEXT_PLAIN)
-//@Produces(MediaType.TEXT_HTML)
-public String readprofile(@PathParam("customerID") String customerID) {
-
-
-
-return customerObj.viewProfile(customerID);
-
-
-}
-
-
  
+              //Delete Customer Details
+ 
+              @DELETE
+              @Path("/")
+              @Consumes(MediaType.APPLICATION_XML)
+              @Produces(MediaType.TEXT_PLAIN)
+              public String deleteCustomer(String CustomerData)
+              {
+               
+              //Convert the input string to an XML document
+               Document doc = Jsoup.parse(CustomerData, "", Parser.xmlParser());
 
-}
+              //Read the value from the element <itemID>
+               String customerID = doc.select("customerID").text();
+               String output = customerObj.deleteCustomer(customerID);
+              return output;
+              }
+
+              //Search Customer Details
+ 
+              @GET
+              @Path("/profile/{customerID}")
+              @Consumes(MediaType.APPLICATION_JSON)
+              @Produces(MediaType.TEXT_PLAIN)
+              //@Produces(MediaType.TEXT_HTML)
+              public String readprofile(@PathParam("customerID") String customerID) {
+
+
+
+              return customerObj.viewProfile(customerID);
+
+
+              }
+
+
+
+
+              }
